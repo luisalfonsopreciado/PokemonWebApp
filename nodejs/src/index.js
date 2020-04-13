f646758ab09943c00e10e008ff1c1a61a632d5c6
Luis Preciado <luisapreciado99@gmail.com>
2020-04-13 14:30:14 -0500

    pokemon router

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -2,31 +2,13 @@ const express = require('express')
 require('./db/mongoose')
 const app = express()
 const userRouter = require('./routes/user.js')
+const pokemonRouter = require('./routes/pokemon.js')
 const port = process.env.PORT || 3000;
-
-const Pokemon = require('./models/Pokemon')
 
 app.use(express.json())
 app.use(userRouter)
-app.get("/pokemon", async(req,res)=> {
-    try{
-        const pokemon = await Pokemon.find({})
-        res.status(200).send(pokemon)
-    }catch(e){
-        res.status(500).send()
-    }
+app.use(pokemonRouter)
 
-})
-
-app.post("/pokemon/create", async(req,res)=> {
-    try{
-        const pokemon = await Pokemon.create(req.body)
-        res.status(200).send(pokemon)
-    }catch(e){
-        res.status(400).send()
-    }
-
-})
 
 app.listen(port, () => {
     console.log("Server listening on port: " + port)
---
+++ nodejs/src/routes/pokemon.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = new express.Router()
+const Pokemon = require('../models/Pokemon.js')
+
+router.get("/pokemon", async(req,res)=> {
+    try{
+        const pokemon = await Pokemon.find({})
+        res.status(200).send(pokemon)
+    }catch(e){
+        res.status(500).send()
+    }
+
+})
+
+router.post("/pokemon/create", async(req,res)=> {
+    try{
+        const pokemon = await Pokemon.create(req.body)
+        res.status(200).send(pokemon)
+    }catch(e){
+        res.status(400).send()
+    }
+
+})
+
+module.exports = router
