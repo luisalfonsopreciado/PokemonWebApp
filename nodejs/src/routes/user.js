cbf95369d52c8df1e82ab837201b36cdda287f59
Luis Preciado <luisapreciado99@gmail.com>
2020-04-13 17:43:09 -0500

    get and add user fav pokemon

--- nodejs/src/routes/user.js
+++ nodejs/src/routes/user.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = new express.Router();
 const User = require("../models/User.js");
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
 
 router.get("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findOne({_id: req.user._id});
+    const user = await User.findOne({ _id: req.user._id });
     res.status(200).send(user);
   } catch (e) {
     res.status(500).send();
@@ -14,7 +14,7 @@ router.get("/users/me", auth, async (req, res) => {
 
 router.delete("/users/me", auth, async (req, res) => {
   try {
-    await req.user.remove()
+    await req.user.remove();
     res.status(200).send("User Deleted Successfully");
   } catch (e) {
     res.status(500).send();
@@ -22,32 +22,34 @@ router.delete("/users/me", auth, async (req, res) => {
 });
 
 router.patch("/users/me", auth, async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ["username", "email"]
-  const isValidOperation = updates.every(update=> allowedUpdates.includes(update))
-  if(!isValidOperation){
-    res.status(400).send({error: "Invalid Updates"})
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["username", "email"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    res.status(400).send({ error: "Invalid Updates" });
   }
   try {
-    updates.forEach(update=> req.user[update] = req.body[update])
-    await req.user.save()
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
     res.status(200).send(req.user);
   } catch (e) {
     res.status(500).send();
   }
 });
 
-router.post("/users/logout",auth, async(req,res) => {
-  try{
+router.post("/users/logout", auth, async (req, res) => {
+  try {
     req.user.tokens = req.user.tokens.filter(
       (token) => token.token !== req.token
     );
     await req.user.save();
-    res.send("Logged out successfully")
-  }catch(e){
-    res.status(500).send()
+    res.send("Logged out successfully");
+  } catch (e) {
+    res.status(500).send();
   }
-})
+});
 
 router.post("/users/login", async (req, res) => {
   try {
@@ -55,30 +57,44 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    const token = await user.generateAuthToken()
-    res.send({user, token});
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
-      res.status(400).send()
+    res.status(400).send();
   }
 });
 
 router.post("/users/create", async (req, res) => {
-  const error={}
+  const error = {};
   try {
-    if(req.body.password1 !== req.body.password2){
-      error.password = "Passwords Must Match"
-      throw new Error()
+    if (req.body.password1 !== req.body.password2) {
+      error.password = "Passwords Must Match";
+      throw new Error();
     }
     const body = {
-      username : req.body.username,
-      email : req.body.email,
-      password : req.body.password1,
-    }
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password1,
+    };
     const user = await User.create(req.body);
     await user.save();
     res.status(201).send(user);
   } catch (e) {
     res.status(400).send(error);
+  }
+});
+
+router.get("/users/favorite", auth, async (req, res) => {
+  res.status(200).send(req.user.favoritePokemons);
+});
+
+router.post("/users/favorite", auth, async (req, res) => {
+  try {
+    req.user.favoritePokemons.push(req.body);
+    await req.user.save();
+    res.status(200).send(req.user.favoritePokemons);
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
