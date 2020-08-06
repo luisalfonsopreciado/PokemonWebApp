312f9cd7847ecb19b4a1cf27b06ebea3fc206fa1
Luis Preciado <luisapreciado99@gmail.com>
2020-08-06 18:48:29 -0500

    poke page

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # poke-api
-![Alt text](gui/resources/images/1.png?raw=true)
 Pokemon App <br>
 Browse and learn about all the available Pokemon!<br>
 React frontend and express backend<br>
--- ssg/pages/pokemon/[id].js
+++ ssg/pages/pokemon/[id].js
@@ -11,6 +11,21 @@ import pokemon from "../../pokemon.json";
 import axios from "axios";
 import Link from "next/link";
 import { TYPE_COLOR } from "../../constants";
+import Chip from "@material-ui/core/Chip";
+import { makeStyles } from "@material-ui/core";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  chip: {
+    margin: "1px",
+  },
+});
 
 export async function getStaticPaths() {
   return {
@@ -33,17 +48,25 @@ export async function getStaticProps(context) {
     "https://pokeapi.co/api/v2/pokemon-form/" + context.params.id
   );
 
+  const { data: encounterData } = await axios.get(
+    // Pokemon locatino encounters
+    res.data.location_area_encounters
+  );
+
   return {
     props: {
       data: pokemon[context.params.id - 1],
       apiData: res.data,
       formData: response.data,
+      encounterData,
     },
   };
 }
 
-export default ({ data, apiData, formData }) => {
+export default ({ data, apiData, formData, encounterData }) => {
   const sprites = Object.keys(formData.sprites);
+  const classes = useStyles();
+
   return (
     <div className="bg-dark">
       <Head>
@@ -67,7 +90,6 @@ export default ({ data, apiData, formData }) => {
               <Col xs={8}>
                 <Row>
                   <h4>Types</h4>
-
                   {apiData.types.map(({ type }, key) => {
                     return (
                       <>
@@ -115,6 +137,15 @@ export default ({ data, apiData, formData }) => {
                   </Col>
                 </Row>
                 <Row>
+                  <Col xs={2}>Height</Col>
+                  <Col xs={10}>
+                    <ProgressBar
+                      now={apiData.height}
+                      label={`${apiData.height}`}
+                    />
+                  </Col>
+                </Row>
+                <Row>
                   <Col xs={2}>Abilities</Col>
                   <Col xs={10}>
                     {apiData.abilities.map(({ ability }, key) => {
@@ -127,6 +158,7 @@ export default ({ data, apiData, formData }) => {
                               ""
                             )
                           }
+                          key={key}
                         >
                           <span className="badge badge-primary badge-pill ml-1 bg-info">
                             {ability.name
@@ -156,50 +188,93 @@ export default ({ data, apiData, formData }) => {
               </Col>
             </Row>
             <Row className="border mt-3 p-2 bg-white rounded">
-              <Col xs={2}>
-                <h3>Sprites</h3>
-                {sprites.map((sprite, key) => {
-                  return <Image src={formData.sprites[sprite]} fluid />;
-                })}
-              </Col>
-              <Col xs={5}>
+              <Col xs={12}>
                 <h3>Moves</h3>
-                {apiData.moves.map(({ move }, key) => {
-                  return (
-                    <Link
-                      href={
-                        "/" + move.url.replace("https://pokeapi.co/api/v2/", "")
-                      }
-                    >
-                      <Alert key={key} variant="primary">
-                        {move.name}
-                      </Alert>
-                    </Link>
-                  );
-                })}
+                <div
+                  style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  {apiData.moves.map(({ move }, key) => {
+                    return (
+                      <Link
+                        href={move.url.replace("https://pokeapi.co/api/v2", "")}
+                      >
+                        <span className="m-1">
+                          <Chip label={move.name} />
+                        </span>
+                      </Link>
+                    );
+                  })}
+                </div>
               </Col>
-              <Col xs={5}>
-                <h2>Abilities</h2>
-                {apiData.abilities.map(({ ability }, key) => {
-                  return (
-                    <Link
-                      href={
-                        "/" +
-                        ability.url.replace("https://pokeapi.co/api/v2/", "")
-                      }
-                    >
-                      <Alert key={key} variant="primary">
-                        {ability.name}
-                      </Alert>
-                    </Link>
-                  );
-                })}
+            </Row>
+            <Row className="border mt-3 p-2 bg-white rounded">
+              <Col xs={12}>
+                <h2 className="text-center">Encounters</h2>
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Location</TableCell>
+                        <TableCell align="right">Max Chance</TableCell>
+                        <TableCell align="right">Pokemon Version</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {encounterData.map(
+                        ({ location_area, version_details }, key) => {
+                          return (
+                            <TableRow key={key}>
+                              <TableCell component="th" scope="row">
+                                {titleCase(location_area.name)}
+                              </TableCell>
+                              <TableCell align="right">1</TableCell>
+                              <TableCell align="right">
+                                {version_details.map(
+                                  ({
+                                    encounter_details,
+                                    max_chance,
+                                    version,
+                                  }) => (
+                                    <span className="m-1">
+                                      <Chip
+                                        label={version.name}
+                                        className={classes.chip}
+                                        color="primary"
+                                        variant="outlined"
+                                      />
+                                    </span>
+                                  )
+                                )}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        }
+                      )}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </Col>
             </Row>
           </>
         )}
-        {/* {JSON.stringify(apiData.moves)} */}
       </Container>
     </div>
   );
 };
+
+function titleCase(str) {
+  str = str.replace(/-/g, " ");
+  var splitStr = str.toLowerCase().split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // Directly return the joined string
+  return splitStr.join(" ");
+}
